a004fc12e480ce05ac32674d501e8da162fe288f
Pathapati Surendra Karun Varma <104403237+SurendraVarma9@users.noreply.github.com>
2022-08-09 16:33:09 +0530

    Update CFileBitReader.java

--- DATA COMPRESSION & DECOMPRESSION/wingph Source Code/src/FileBitIO/CFileBitReader.java
+++ DATA COMPRESSION & DECOMPRESSION/wingph Source Code/src/FileBitIO/CFileBitReader.java
@@ -1,11 +1,4 @@
-/*
- * CFileBitReader.java
- *
- * Created on May 7, 2009, 2:07 PM
- *
- * To change this template, choose Tools | Template Manager
- * and open the template in the editor.
- */
+
 
 package FileBitIO;
 import java.io.*;
